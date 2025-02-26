319b790b7ba2fff53d8e57818083dc7b56a88215
Ahmed Hussain <a.hmedhussain@hotmail.com>
2025-02-26 17:04:06 +0000

    adding springboot to project

---
+++ marvApiJava/src/test/java/com/example/marvApiJava/MarvApiJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.marvApiJava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarvApiJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
