c63825d70a6c92c3ac28fa4ba00e59557a223862
Ahmed Hussain <a.hmedhussain@hotmail.com>
2025-02-27 16:24:10 +0000

    adding async function to the main and can display images of heroes from fetched get reqfrom server

--- marvology-website/src/main.ts
+++ marvology-website/src/main.ts
@@ -1,24 +1,20 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+import './style.scss'
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+const heroImgsContainer = document.querySelector<HTMLInputElement>('.heroes-container')
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+if(!heroImgsContainer){
+    throw new Error('oculdnt find it!')
+}
+
+const handleImgUpdate = async () => {
+    const response = await fetch("http://localhost:8080/heroes/")
+    const arrOfHeroes = await response.json();
+    console.log(arrOfHeroes)
+    arrOfHeroes.forEach(element => {
+        const heroImg = document.createElement("img")
+        heroImgsContainer.appendChild(heroImg)
+        heroImg.src = element.image_url
+    });
+}
+
+handleImgUpdate()
