2f0bc79611cc052d36adb666a4a2b2ffd5c7664b
Ahmed Hussain <a.hmedhussain@hotmail.com>
2025-02-27 17:41:59 +0000

    added a toggle to view hero information when image of hero is clicked

--- marvology-website/src/main.ts
+++ marvology-website/src/main.ts
@@ -6,15 +6,59 @@ if(!heroImgsContainer){
     throw new Error('oculdnt find it!')
 }
 
+type Hero = {
+    id : number,
+    alias: string,
+    image_url: string,
+    real_name: string,
+    description: string
+}
+
+const handleImgClick = (hero: Hero, heroContainer: HTMLDivElement) => {
+    console.log(hero.alias)
+    console.log(hero.id)
+
+    const heroText = heroContainer.querySelector(`.hero-text`)
+    
+    if(!heroText){
+        const heroText = document.createElement('div')
+        heroText.className = `hero-text`
+        heroContainer.appendChild(heroText)
+
+            const heroRealName = document.createElement('p')
+            heroRealName.innerText = hero.real_name;
+            
+            const heroAlias = document.createElement('p')
+            heroAlias.innerText = hero.alias;
+            
+            const heroDesc = document.createElement('p')
+            heroDesc.innerText =  hero.description;
+            
+            heroText.appendChild(heroRealName)
+            heroText.appendChild(heroAlias)
+            heroText.appendChild(heroDesc)
+    } else {
+        heroText.remove()
+    }
+
+}
+
 const handleImgUpdate = async () => {
     const response = await fetch("http://localhost:8080/heroes/")
     const arrOfHeroes = await response.json();
     console.log(arrOfHeroes)
-    arrOfHeroes.forEach(element => {
+    arrOfHeroes.forEach((hero:Hero) => {
+        const heroContainer = document.createElement('div')
+        heroImgsContainer.appendChild(heroContainer)
+        heroContainer.className = 'singleHeroContainer'
         const heroImg = document.createElement("img")
-        heroImgsContainer.appendChild(heroImg)
-        heroImg.src = element.image_url
+        heroContainer.appendChild(heroImg)
+        heroImg.src = hero.image_url
+        heroImg.addEventListener("click",() => handleImgClick(hero,heroContainer))
     });
 }
 
+
+
 handleImgUpdate()
+
