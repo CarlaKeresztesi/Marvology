8cc0cfd4a6ececeea8ad3d3c05e185c7e85dae1c
CarlaKeresztesi <carlakeresztesi@gmail.com>
2025-02-28 11:40:41 +0000

    Worked on functionality of search button for movies

--- marvology-website/src/main.ts
+++ marvology-website/src/main.ts
@@ -3,8 +3,9 @@ import './style.scss'
 const heroImgsContainer = document.querySelector<HTMLInputElement>('.heroes-container')
 const movieInputBox = document.querySelector<HTMLInputElement>('#moviesInput')
 const moviesForm = document.querySelector<HTMLFormElement>('#moviesForm')
+const moviesSelected = document.querySelector<HTMLDivElement>('.movies-container--selectedMovies')
 
-if(!heroImgsContainer || !movieInputBox || !moviesForm){
+if(!heroImgsContainer || !movieInputBox || !moviesForm || !moviesSelected){
     throw new Error('couldnt find it!')
 }
 
@@ -24,9 +25,27 @@ type Movie = {
 
 const handleMovieSearch = async (event : SubmitEvent) => {
     event?.preventDefault();
+    moviesSelected.innerHTML = "";
+    const response = await fetch("http://localhost:8080/movies/");
+    const arrOfMovies = await response.json();
     const inputValue = movieInputBox.value;
         console.log(inputValue);
         console.log("movieSearch");
+        console.log(arrOfMovies);
+        arrOfMovies.forEach((movie : Movie) => {
+            const newInput = inputValue.toLowerCase();
+            const newTitle = movie.title.toLowerCase();
+            console.log("not in the if")
+            console.log(newInput)
+            console.log(movie.title)
+            if (newTitle.includes(newInput)) {
+                const movieText = document.createElement('p');
+                movieText.className = 'movie-text';
+                moviesSelected.appendChild(movieText);
+                movieText.innerText = `${movie.title}`;
+                console.log("in if stat")
+            }
+        } )
 }
 
 const handleImgClick = (hero: Hero, heroContainer: HTMLDivElement) => {
